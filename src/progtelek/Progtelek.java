00f639f2ab4764935a7deb7763ff47068ecef780
Yclaze <teralocsi@gmail.com>
2024-05-25 14:24:16 +0200

    impl void progtetelek()

--- src/progtelek/Progtelek.java
+++ src/progtelek/Progtelek.java
@@ -8,6 +8,40 @@ public class Progtelek {
     }
 
     private static void progtetelek() {
+        osszegzes();
+        megszamolas();
+        eldontes();
+        kivalasztas();
+        kereses();
+        maxKivalasztas();
+        minKivalasztas();
+    }
+
+    private static void osszegzes() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void megszamolas() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void eldontes() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void kivalasztas() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void kereses() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void maxKivalasztas() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void minKivalasztas() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
