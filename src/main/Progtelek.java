4c59bbfca1afa1b17864b4fff4bfdd0328ee1e5f
Yclaze <teralocsi@gmail.com>
2024-05-25 15:48:14 +0200

    konzolraIr()

--- src/main/Progtelek.java
+++ src/main/Progtelek.java
@@ -4,17 +4,22 @@ package main;
 public class Progtelek {
 
     public static int[] tomb = new int[]{2, -4, 7, 11, 0};
+    private static int a, b, c, d;
     
     public static void main(String[] args) {
         progtetelek();
     }
 
     private static void progtetelek() {
+        a = 5;
+        b = 2;
+        c = 7;
+        d = 0;
         osszegzes();
-        megszamolas(5); //nagyobb, mint megadott paraméter
-        eldontes(2); //megadott paraméter szerepel-e a tömbben
-        kivalasztas(7); //megadott paraméter hol szerepel a tömbben
-        kereses(0); //megadott paraméter hol szerepel a tömbben indexxel
+        megszamolas(a); //nagyobb, mint megadott paraméter
+        eldontes(b); //megadott paraméter szerepel-e a tömbben
+        kivalasztas(c); //megadott paraméter hol szerepel a tömbben
+        kereses(d); //megadott paraméter hol szerepel a tömbben indexxel
         maxKivalasztas();
         minKivalasztas();
         konzolraIr();
@@ -27,6 +32,18 @@ public class Progtelek {
             tombSzoveg += "%d ".formatted(elem);
         }
         System.out.print("A tömb elemei: [ " + tombSzoveg + "]\n");
+        System.out.printf("Tömb összege: %d\n", osszegzes());
+        System.out.printf("Nagyobb mint %d: %d db\n", a, megszamolas(a));
+        String valasz = eldontes(b) ? "Igen" : "Nem"; 
+        System.out.printf("Szerepel-e a(z) %d? %s\n", b, valasz);
+        System.out.printf("Hányadik elem a %d? %d-ik\n", c, kivalasztas(c));
+        if (kereses(d) != -1) {
+            System.out.printf("Mi az indexe a %d-nek? %d\n", d, kereses(d));
+        }else{
+            System.out.printf("Mi az indexe a %d-nek? %s\n", d, "Nincs ilyen elem a tömbben.");
+        }
+        System.out.printf("Maximuma: %d\n",maxKivalasztas());
+        System.out.printf("Minimuma: %d\n",minKivalasztas());
     }
 
     public static int osszegzes() {
@@ -88,7 +105,7 @@ public class Progtelek {
     public static int minKivalasztas() {
         int min = tomb[0];
         for (int szam : tomb) {
-            if (szam > min) {
+            if (szam < min) {
                 min = szam;
             }
         }        
