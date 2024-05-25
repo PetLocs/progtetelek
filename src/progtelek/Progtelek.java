87583232f5ac9752509ac22c89abc009f51bcd50
Yclaze <teralocsi@gmail.com>
2024-05-25 14:19:31 +0200

    progtetelek

---
+++ src/progtelek/Progtelek.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package progtelek;
+
+/**
+ *
+ * @author Péter
+ */
+public class Progtelek {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
