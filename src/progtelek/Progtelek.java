e31a739579b5ea22bfc96f4747e7d9ac5fa35a1e
Yclaze <teralocsi@gmail.com>
2024-05-25 14:21:52 +0200

    void progtetelek()

--- src/progtelek/Progtelek.java
+++ src/progtelek/Progtelek.java
@@ -4,7 +4,11 @@ package progtelek;
 public class Progtelek {
 
     public static void main(String[] args) {
-        
+        progtetelek();
+    }
+
+    private static void progtetelek() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
